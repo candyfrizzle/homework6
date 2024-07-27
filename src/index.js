d7d75f1acb454aa541d7e6b4fa56459cb3564b72
candyfrizzle <patnarh027@gmail.com>
2024-07-27 18:53:21 +0000

    add all new files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 function hello(){let name = prompt("What is your name");
     let hey = document.getElementById("hey");
-    hey.innerHTML=(`Hi ${name} welcome to my website`);
+    hey.innerHTML=(`Hi ${name} welcome to African choise`);
 
 }
 
 let hey=document.getElementById("Hey");
 hey.addEventListener("mouseenter",hello);
+function changeColor(){
+    let name = promt("what is your name");
+    let head=document.getElementById("head");
+    //head.style.overscrollBehaviour="hover";
+    if(name===null){
+        head.style.cursor="pointer";
+        head.innerHTML=`👋👋 Hi there Welcome to African Choice `;
+        let browser=document.getElementById("browse");
+        browser.innerHTML=`Welcome there Taste  The Raw Goodness Of Africa`;
+
+    }else{
+        head.style.cursor="pointer";
+        head.innerHTML=` 👋👋Hi ${name}Welcome to Africa Choice`;
+        let browse=document.getElementById("browse");
+        browse.innerHTML=`Welcome ${name} Taste The Raw Goodness Of Africa`;
+
+    }
+}
+letlinking=document.getElementById("linking");
+linking.addEventListener(`mouseenter`,changeColor);
