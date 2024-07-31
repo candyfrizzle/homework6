395584a45be88731f7cdec964d8e9d5fb1070f62
candyfrizzle <patnarh027@gmail.com>
2024-07-31 23:17:47 +0000

    Add all files

---
+++ src/product.js
@@ -0,0 +1,72 @@
+function Showtext(){
+    let old =document.getElementById("old");
+    old.style.display="inline";
+    let land =document.getElementById("readtwo");
+    land.style.display="none";
+    let landee=document.getElementById("lessone");
+    landee.style.display=("block");
+}
+let ring =document.getElementById("readtwo");
+ring.addEventListener("click",Showtext);
+
+
+
+
+function Hidetext(){
+    let zip =document.getElementById("zip");
+    zip.style.display="inline";
+    let come=document.getElementById("run");
+    come.style.display="none";
+    let comee=document.getElementById("lessone");
+    comee.style.display=("block");
+}
+let rat =document.getElementById("run");
+rat.addEventListener("click",Hidetext);
+
+
+
+function hidetexttwo(){
+    let zoom =document.getElementById("zoom");
+    zoom.style.display="inline";
+    let dos=document.getElementById("some");
+    dos.style.display="none";
+    let does=document.getElementById("lessone");
+    does.style.display=("block");
+}
+let lane =document.getElementById("some");
+lane.addEventListener("click",hidetexttwo);
+
+
+function hidetextone(){
+    let laptop =document.getElementById("laptop");
+    laptop.style.display="inline";
+    let lock=document.getElementById("done");
+    lock.style.display="none";
+    let cat=document.getElementById("lessone");
+    cat.style.display=("block");
+}
+let rich =document.getElementById("done");
+rich.addEventListener("click",hidetextone); 
+
+function Showbhim(){
+    let bond =document.getElementById("bond");
+    bond.style.display="inline";
+    let call=document.getElementById("Profel");
+    call.style.display="none";
+    let song=document.getElementById("lessone");
+    song.style.display=("block");
+}
+let hate =document.getElementById("Profel");
+hate.addEventListener("click",Showbhim);
+
+
+function hidethree(){
+    let dotted =document.getElementById("dotted");
+    dotted.style.display="inline";
+    let feel=document.getElementById("food");
+    feel.style.display="none";
+    let back=document.getElementById("lessone");
+    back.style.display=("block");
+}
+let last=document.getElementById("food");
+last.addEventListener("click",hidethree);
